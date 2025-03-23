8551259a38e5aae4ef21db2dc4a127a537762d50
Adedeji2563 <adedejiadesida1@gmail.com>
2025-03-23 10:37:18 +0100

    Update rateLimit.js

--- middleware/rateLimit.js
+++ middleware/rateLimit.js
@@ -1,7 +1,9 @@
 const rateLimit = require("express-rate-limit");
 
-module.exports = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per window
   message: "Too many requests, try again later."
 });
+
+module.exports = limiter;
